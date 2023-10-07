996e1c7a26d26ba7f2b9ab2073cf9cf6be2fc1a8
Olivia-Pigani <piganiolivia@gmail.com>
2023-10-07 19:31:42 +0200

    right about making the modal component, wich is the children of Display component

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,5 +1,18 @@
+import svg from "../assets/img/card-checklist.svg"
+
 const Display = (props) => {
   const { data, setRecettes } = props;
+
+
+    const descriptionLogoStyle = {
+        width:"40px",
+        height:"40px",
+        backgroundColor:"orange",
+        borderRadius:"50%",
+        border:"none"
+
+
+    }
 
   console.log(data);
  
@@ -10,11 +23,15 @@ const Display = (props) => {
     console.log(id)
   };
 
+ 
+
   return (
     <div>
       {data.map((recette, index) => (
         <div key={index}>
           {recette.title}
+            <button style={descriptionLogoStyle} ><img src={svg} alt="details Button" /></button>
+     
           <button onClick={() => deleteItemClick(recette.id)}>delete</button>
         </div>
       ))}
